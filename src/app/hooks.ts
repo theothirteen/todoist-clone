f891015261de3b742c8c47af4f0055790153091f
theothirteen <urvish0013@gmail.com>
2022-01-12 20:22:53 +0530

    Integrated Redux

---
+++ src/app/hooks.ts
@@ -0,0 +1,26 @@
+import type { ChangeEvent } from 'react';
+import { useEffect, useRef } from 'react';
+import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
+
+import type { AppDispatch, AppState } from './store';
+
+// https://overreacted.io/making-setinterval-declarative-with-react-hooks/
+export const useInterval = (callback: Function, delay: number) => {
+  const savedCallback = useRef<Function>();
+  useEffect(() => {
+    savedCallback.current = callback;
+  }, [callback]);
+  useEffect(() => {
+    const handler = (...args: any) => savedCallback.current?.(...args);
+
+    if (delay !== null) {
+      const id = setInterval(handler, delay);
+      return () => clearInterval(id);
+    }
+  }, [delay]);
+};
+
+// Use throughout your app instead of plain `useDispatch` and `useSelector`
+export const useAppDispatch = () => useDispatch<AppDispatch>();
+
+export const useAppSelector: TypedUseSelectorHook<AppState> = useSelector;
